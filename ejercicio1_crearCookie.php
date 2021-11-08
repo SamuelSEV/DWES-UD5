0bcda410f977fa91a44deefd4be2a3cbd9f7cc47
SamuelSEV <samuelsev26@gmail.com>
2021-11-09 00:27:10 +0100

    ejercicio 1

---
+++ ejercicio1_crearCookie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <?php
+
+    setcookie("Nombre", "Samuel Rivera Peñalosa", time() + 10800, "/");
+    setcookie("Edad", 35, time() + 10800, "/");
+
+    ?>
+</body>
+</html>
